5979dc3e2a520fb89f323042afcc32fc0b24b5b8
Sami-Juhani <sami.paananen@gmail.com>
2024-09-27 15:26:59 +0300

    (dev): Route to download CV

---
+++ src/app/api/download/cv/route.ts
@@ -0,0 +1,17 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+export async function GET() {
+  // process.cwd() is the root of the Next.js app
+  const buffer = await readFile(path.join(process.cwd(), "public/assets/author/CV_Sami_Paananen.pdf"));
+
+  // set the headers to tell the browser to download the file
+  const headers = new Headers();
+  // remember to change the filename `test.pdf` to whatever you want the downloaded file called
+  headers.append("Content-Disposition", 'attachment; filename="CV_Sami_Paananen.pdf"');
+  headers.append("Content-Type", "application/pdf");
+
+  return new Response(buffer, {
+    headers,
+  });
+}
