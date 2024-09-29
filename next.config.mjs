75edb50ed3f1fa787e41747e80f838c0f92f331e
Sami-Juhani <sami.paananen@gmail.com>
2024-09-29 11:39:22 +0300

    (dev): Add X-Robots-Tag: noindex on preview deployment with custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    poweredByHeader: false
+    poweredByHeader: false,
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+          headers.push({
+            headers: [
+              {
+                key: 'X-Robots-Tag',
+                value: 'noindex',
+              },
+            ],
+            source: '/:path*',
+          });
+        }
+        return headers;
+      },
 };
 
 export default nextConfig;
