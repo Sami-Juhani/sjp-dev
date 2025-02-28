55a0500016a34bcde76162309479d622e403b8ab
SamiJuhani <sami.paananen@gmail.com>
2025-02-28 22:50:03 +0200

    (fix): update imports

--- src/app/[lang]/(admin)/_lib/services/ai.ts
+++ src/app/[lang]/(admin)/_lib/services/ai.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { createOpenAI } from '@ai-sdk/openai'
+import { createOpenAI, openai } from '@ai-sdk/openai'
 import { generateText } from 'ai'
 
 const API_KEY = process.env.OPENAI_API_KEY
@@ -26,7 +26,7 @@ export async function aiTranslateText(
     const { text } = await generateText({
       system:
         'You are a helpful assistant that translates English Markdown files to Finnish and vice versa. Return the results in the exact same Markdown format as the original, without marking it as a code block or adding additional syntax. Escape special characters in code blocks, and ensure the output follows standard Markdown formatting.',
-      model: openAIClient('gpt-4o-mini'),
+      model: openai('gpt-4o-mini'),
       prompt
     })
 
